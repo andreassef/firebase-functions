f4696c298459405babb55e5d16bbc68c0769b2e2
Andre Assef <andreassef02@gmail.com>
2020-10-01 14:54:52 -0400

    algoritmo que avalia uma palavra específica e a substitui por caracteres aleatorios

--- functions/index.js
+++ functions/index.js
@@ -6,27 +6,34 @@ const db = admin.database();
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
 
- exports.helloWorld = functions.https.onRequest((request, response) => {
-   functions.logger.info("Hello logs!", {structuredData: true});
-   response.send("Primeiro codigo com google functions!");
-});
+// Singela aplicação simuladora de um algoritmo que substitui palavrões ao serem inseridos no 
+// realtime database. Feita com ajudo do firebase e cloud functions
 
-exports.addMessage = functions.https.onRequest( async (req, res) =>{
-
-    const textParam = req.query.text;
-
-    // 
-    const writeResult = await admin.firestore().collection('messages').add({original: textParam});
-
-    res.json({result: `Message with ID: ${writeResult.id} added.`});
-})
-
+// adiciona um novo objeto no realtime database com nome, mensagem e time.
 exports.newMessage = functions.https.onRequest( async (req, res) => {
   const { nome, mensagem, time } = req.body;
-  //const newUser = db.ref().child('mensagem').push().key;
-
+  
   const usuario = db.ref('root').child('usuarios').child('mensagens');
   await usuario.push({nome, mensagem, time});
-
+  
   res.json({resultado: 'sucesso ao salvar dados!'});
 })
+
+// Gatilho do realtime database, toda vez que uma nova mensagem for inserida no path
+// a funcao verifyAwfulWord é chamada e procura a palavra perjorativa "porra" que ao ser encontrada é substituida
+// por caracteres aleatorios
+exports.searchAwfulWords = functions.database.
+ref('/root/{usuarios}/mensagens/{mensagemId}')
+.onCreate((snapshot, context) => {
+  const dadosMensagem = snapshot.val();
+  console.log(dadosMensagem);
+  const text = verifyAwfulWord(dadosMensagem.mensagem); //string
+  return snapshot.ref.update({mensagem: text});
+})
+
+// função  verifica um palavrao e por meio da Regex substitui por um caractere aleatorio.
+function verifyAwfulWord(text) {
+  return text.replace(/\bporra\b/g, '*#@!!**');
+}
+
+
